816517cb5fe26d94402c285290d8e5dca22877f6
Al1tvin <lesha.litvin2706@gmail.com>
2024-11-09 13:05:52 +0300

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -1,9 +1,10 @@
-def print_parasm(a = 1, b = 'Строка', c = True):
+def print_params(a = 1, b = 'Строка', c = True):
     print(a,b,c)
-print_parasm()
+print_params(b = 25)
+print_params(c = [1, 2, 3])
 values_list = [12,'str',False]
-valuse_dict = {'a' : 20, 'b' : 'Стр' , 'c': True}
-print_parasm(*values_list)
-print_parasm(**valuse_dict)
+values_dict = {'a' : 20, 'b' : 'Стр' , 'c': True}
+print_params(*values_list)
+print_params(**values_dict)
 values_list_2=['str',10]
-print_parasm(*values_list_2)
+print_params(*values_list_2)
