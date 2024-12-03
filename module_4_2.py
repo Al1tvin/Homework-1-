b6f5a2d3076b69fb376aedb24fe6f78c5ed36852
Al1tvin <lesha.litvin2706@gmail.com>
2024-12-03 19:24:06 +0300

    Update module_4_2.py

--- module_4_2.py
+++ module_4_2.py
@@ -3,5 +3,8 @@ def test_function():
         print('Я в области видимости test_function')
 
     inner_function()
+    print(inner_function)
 
 test_function()
+test_function()() # Вызов внутренней функции через внешнюю
+inner_function()  # Не можем вызвать inner_function т.к. она находится в локальной области test_function
